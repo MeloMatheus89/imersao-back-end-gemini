c7e28c34a1465f2318136a6e3d67d66417fe6e03
MeloMatheus89 <156861214+MeloMatheus89@users.noreply.github.com>
2024-11-20 12:17:32 -0300

    criando lógica para procurar por ID do objeto da postagem e adaptando o get posts para buscar pelo id

--- server.js
+++ server.js
@@ -1,10 +1,60 @@
 //arquivo do servidor
 import express from "express";
 
+const posts = [
+  {
+    id: 1,
+    descricao: "Uma foto teste",
+    imagem: "https://placecats.com/millie/300/150",
+  },
+  {
+    id: 2,
+    descricao: "Gato preguiçoso tomando sol",
+    imagem: "https://placecats.com/millie/300/150",
+  },
+  {
+    id: 3,
+    descricao: "Paisagem montanhosa incrível",
+    imagem: "https://placecats.com/millie/300/150",
+  },
+  {
+    id: 4,
+    descricao: "Cachorro brincando na praia",
+    imagem: "https://placecats.com/millie/300/150",
+  },
+  {
+    id: 5,
+    descricao: "Comida deliciosa",
+    imagem: "https://placecats.com/millie/300/150",
+  },
+  {
+    id: 6,
+    descricao: "Citação inspiradora",
+    imagem: "https://placecats.com/millie/300/150",
+  },
+  {
+    id: 7,
+    descricao: "Arte abstrata colorida",
+    imagem: "https://placecats.com/millie/300/150",
+  },
+];
+
 const app = express();
+
+app.use(express.json());
 app.listen(3000, () => {
   console.log("Servidor escutando");
 });
-app.get("/api", (req, res) => {
-  res.status(200).send("Boas vindas à imersão");
+app.get("/posts", (req, res) => {
+  res.status(200).json(posts);
+});
+
+function buscarPorID(id) {
+  return posts.findIndex((post) => {
+    return post.id === Number(id);
+  });
+}
+app.get("/posts/:id", (req, res) => {
+  const index = buscarPorID(req.params.id);
+  res.status(200).json(posts[index]);
 });
