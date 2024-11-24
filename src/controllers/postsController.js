5d249d929aa4706b96ae648dd3805a5cdbcc9c34
MeloMatheus89 <156861214+MeloMatheus89@users.noreply.github.com>
2024-11-24 17:28:46 -0300

    renomeando o arquivo com o nome aleatório

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,3 +1,4 @@
+import fs from "fs";
 import { getTodosPosts, criarPost } from "../models/postModel.js";
 export async function listarPosts(req, res) {
   // Chama a função getTodosPosts() para obter todos os posts
@@ -22,6 +23,8 @@ export async function uploadImagem(req, res) {
   const novoPost = req.body;
   try {
     const postCriado = await criarPost(novoPost);
+    const imagemAtualizada = `uploads/${postCriado.insertedId}.png`;
+    fs.renameSync(req.file.path, imagemAtualizada);
     res.status(200).json(postCriado);
   } catch (erro) {
     console.error(erro.message);
