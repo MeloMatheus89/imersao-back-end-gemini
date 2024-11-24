69ccefb1231dd6f50d2d7648bb599bcb6dfc0a10
MeloMatheus89 <156861214+MeloMatheus89@users.noreply.github.com>
2024-11-24 14:09:17 -0300

    incluindo o multer e criação da pasta upload

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,22 @@
 import express from "express";
-import { listarPosts, postarNovoPost } from "../controllers/postsController.js";
+import multer from "multer";
+import {
+  listarPosts,
+  postarNovoPost,
+  uploadImagem,
+} from "../controllers/postsController.js";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ dest: "./uploads", storage });
+//linux ou mac ele nomeia automaticamente o nome onde será salvo.
 
 const routes = (app) => {
   // Habilita o parsing de JSON no corpo das requisições
@@ -8,6 +25,7 @@ const routes = (app) => {
   app.get("/posts", listarPosts);
   // Define uma rota para criar um novo Post
   app.post("/posts", postarNovoPost);
+  app.post("/upload", upload.single("imagem"), uploadImagem);
 };
 
 export default routes;
